f6f384d923481c104e08b4715edfff92edb928c9
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2020-11-12 09:44:46 -0600

    nice message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello git last day");
+        System.out.println("we create some change");
+        //b21 exited
     }
 }
